a28e0e105193edece47f39ab78d36ae803cbf6e7
C4164 <c4164@oic.jp>
2026-06-17 12:50:14 +0900

    小役の種類の修正

--- Slot_NormalType/Consts.h
+++ Slot_NormalType/Consts.h
@@ -5,13 +5,15 @@ namespace Const
 	//小役の種類
 	constexpr enum MinorPrize
 	{
-		bell,			//ベル
-		replay,			//リプレイ
-		chance,			//チャンス目
-		melon_weakness,	//弱スイカ
-		melon_strength,	//強スイカ
-		cherry_weakness,//弱チェリー
-		cherry_strength,//強チェリー
+		bell,				//ベル
+		replay,				//リプレイ
+		chance,				//チャンス目
+		melon_weakness,		//弱スイカ
+		melon_strength_1,	//強スイカ1
+		melon_strength_2,	//強スイカ2
+		cherry_weakness,	//弱チェリー
+		cherry_strength_1,	//強チェリー1
+		cherry_strength_2,	//強チェリー2
 	};
 
 	//設定の段階(数)
@@ -24,8 +26,10 @@ namespace Const
 		const int REPLAY[SETTING] = { 0,0,0,0,0,0 };			//リプレイ
 		const int CHANCE[SETTING] = { 0,0,0,0,0,0 };			//チャンス目
 		const int MELON_WEAKNESS[SETTING] = { 0,0,0,0,0,0 };	//弱スイカ
-		const int MELON_STRENGTH[SETTING] = { 0,0,0,0,0,0 };	//強スイカ
+		const int MELON_STRENGTH_1[SETTING] = { 0,0,0,0,0,0 };	//強スイカ1
+		const int MELON_STRENGTH_2[SETTING] = { 0,0,0,0,0,0 };	//強スイカ2
 		const int CHERRY_WEAKNESS[SETTING] = { 0,0,0,0,0,0 };	//弱チェリー
-		const int CHERRY_STRENGTH[SETTING] = { 0,0,0,0,0,0 };	//強チェリー
+		const int CHERRY_STRENGTH_1[SETTING] = { 0,0,0,0,0,0 };	//強チェリー1
+		const int CHERRY_STRENGTH_2[SETTING] = { 0,0,0,0,0,0 };	//強チェリー2
 	};
 }
